dc72fb092be03f6a590ea7aa13b981b44ceb63c0
tom <tom@localhost.localdomain>
2017-02-14 21:15:34 +0000

    Added a sample component.

--- src/ReactBaseSetup/src/index.js
+++ src/ReactBaseSetup/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {SampleComp} from './sample_component';
 
 ReactDOM.render(
   <div>
     <h1>Base ReactJS Template</h1>
+    <SampleComp />
   </div>
   ,document.getElementById('root')
 );
---
+++ src/ReactBaseSetup/src/sample_component.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SampleComp extends Component {
+  render() {
+    return <h2>Here is a sample component.</h2>;
+  }
+}
+
+export {SampleComp};
